ddc994935cc4975d716aed0121c3df6265368568
murarikrishna007 <murarikrishna0071994@gmail.com>
2022-02-12 10:39:45 +0530

    digital_fueled website build in html and css

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// console.log("hello krishna");
+
+// toggle menu button js
+
+let navbar = document.getElementById("navBar");
+
+function togglebtn(){
+    navbar.classList.toggle("hidemenu");
+}
